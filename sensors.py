10c81e88588ffd2323b75dd3ad7f10631c3134b5
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2022-01-08 13:03:01 +0200

    Merge branch 'main' of github.com:euxoa/sensors into main

--- sensors.py
+++ sensors.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import time, colorsys, sys, os, pickle, itertools
+import time, colorsys, sys, os, pickle, itertools, asyncio
 import numpy as np
 
 import ST7735
@@ -16,24 +16,6 @@ from fonts.ttf import RobotoMedium as UserFont
 INTEGRATORS_FILENAME = "integrators.pyobj"
 LOG_FILENAME = "3.txt"
 
-def Enviro_plus():
-    ltr559 = LTR559() # light
-    ltr559.set_light_options(gain=96)
-    ltr559.set_light_integration_time_ms(400)
-    bme280 = BME280() # temp etc
-    pms5003 = PMS5003() # particulates
-    time.sleep(1.0) # seems like delicate stuff, the thing above
-
-    while True:
-     yield (ltr559.get_proximity(),
-            ltr559.get_lux(),
-            bme280.get_temperature(),
-            bme280.get_humidity(),
-            bme280.get_pressure(),
-            pms5003.read(), # pms5003.ChecksumMismatchError
-            gas.read_all())
-
-    
 # Create display instance
 st7735 = ST7735.ST7735(port=0, cs=1, dc=9, backlight=12, rotation=270,
                        spi_speed_hz=10000000)
@@ -99,7 +81,25 @@ class Anom:
     def uz2(self, v):
         self.update(v)
         return self.z2(v)
-    
+
+async def Enviro_plus(interval = 1.0):
+    ltr559 = LTR559() # light
+    ltr559.set_light_options(gain=96)
+    ltr559.set_light_integration_time_ms(400)
+    bme280 = BME280() # temp etc
+    pms5003 = PMS5003(); time.sleep(1.0) # particulates; seems somewhat delicate
+    t_sync = time.time()
+    while True:
+        yield (time.time(),
+               ltr559.get_proximity(),
+               ltr559.get_lux(),
+               bme280.get_temperature(),
+               bme280.get_humidity(),
+               bme280.get_pressure(),
+               pms5003.read(), # pms5003.ChecksumMismatchError
+               gas.read_all())
+        await asyncio.sleep(interval - (time.time() - t_sync) % interval)
+
 
 if os.path.isfile(INTEGRATORS_FILENAME):
         print("Reading integrators.")
@@ -111,63 +111,68 @@ else:
         a_lng = Anom(10, .0001) # half time about five days (sample per minute)
 
 logfile = open(LOG_FILENAME, "a")
-xs = []
-v_lngs = []
+envp = Enviro_plus()
 
-for i, measurements in enumerate(Enviro_plus()):
-    proximity, lux, temp, humidity, pressure, pms_obj, gas_obj = measurements
-    
-    # x are for logging and display
-    x_gas = tuple([7 - np.log10(x) for x in (gas_obj.oxidising, gas_obj.reducing, gas_obj.nh3)])
-    x_pms = tuple([pms_obj.pm_ug_per_m3(x) for x in (1.0, 2.5, 10.0)])
+async def main():
+    xs = []
+    v_lngs = []
+    for i in itertools.count():
+        t0, proximity, lux, temp, humidity, pressure, pms_obj, gas_obj = await envp.asend(None)
 
-    # v are for anomalies etc
-    v_gas = np.array(x_gas)
-    v_pms = np.array(np.log1p(x_pms))
-    v_env = np.array((temp-20, humidity-50))
-    v_prs = np.array((pressure-1000,))
-    v_lgt = np.array((np.log(lux+.01),))
+        # x are for logging and display
+        x_gas = tuple([7 - np.log10(x) for x in (gas_obj.oxidising, gas_obj.reducing, gas_obj.nh3)])
+        x_pms = tuple([pms_obj.pm_ug_per_m3(x) for x in (1.0, 2.5, 10.0)])
 
-    # Display
-    if i>1: # First reading(s) bogus, skip them
-        z_act = a_act.uz2(np.concatenate((v_lgt, v_env, v_gas)))
-        z_env = a_env.uz2(np.concatenate((v_env, v_gas)))
-        z_gpm = a_gpm.uz2(np.concatenate((v_prs, v_pms, v_env)))
-        if (False):
-            print(z_act, z_env, z_gpm)
-            #print(np.linalg.eigvals(a_act.SS))
-            #print(np.concatenate((v_lgt, v_env, v_gas)))
-            #print(a_act.N, a_act.mean)
-        # z with mean 1, sd \appr .6, so 2 is below two-sigma
-        # properly, this shoudl still take df in, do a chisq thing
-        def barify(z): return int(min(255, 256*max(0, z-2)/4)) 
-            
-        display_everything({'temp': temp, 'hum' : humidity, 'pres' : pressure,
-                            'pms010': x_pms[0], 'pms025': x_pms[1], 'pms100': x_pms[2] },
-                           bars = ((barify(z_act), 0, 0),
-                                   (0, barify(z_env), 0),
-                                   (0, 0, barify(z_gpm))),
-                           bg  = (0, 0, 0)) #(255*(proximity>0), 0, 0))
+        # v are for anomalies etc
+        v_gas = np.array(x_gas)
+        v_pms = np.array(np.log1p(x_pms))
+        v_env = np.array((temp-20, humidity-50))
+        v_prs = np.array((pressure-1000,))
+        v_lgt = np.array((np.log(lux+.01),))
 
-        # Logging
-        x = np.array((time.time(), temp, pressure, humidity, lux, proximity) +
-                         x_gas + x_pms + (z_act,), 'float')
-        xs.append(x) 
-        v_lngs.append(np.concatenate((v_gas, v_env, v_prs, v_pms, v_lgt)))
+        # Display
+        if i>1: # First reading(s) bogus, skip them
+            z_act = a_act.uz2(np.concatenate((v_lgt, v_env, v_gas)))
+            z_env = a_env.uz2(np.concatenate((v_env, v_gas)))
+            z_gpm = a_gpm.uz2(np.concatenate((v_prs, v_pms, v_env)))
+            if (True):
+                pass
+                # print(time.time())
+                # print(v_lgt, v_env, v_gas)
+                # print(z_act, z_env, z_gpm)
+                # print(np.linalg.eigvals(a_act.SS))
+                # print(np.concatenate((v_lgt, v_env, v_gas)))
+                # print(a_act.N, a_act.mean)
+            # z with mean 1, sd \appr .6, so 2 is below two-sigma
+            # properly, this shoudl still take df in, do a chisq thing
+            def barify(z): return int(min(255, 256*max(0, z-2)/4)) 
 
-    if i % 60 == 0 and i>1: # Skip the round with no data
-        xm = np.mean(xs, 0); xs = []
-        v_lng = np.mean(v_lngs, 0); v_lngs = []
-        z_lng = a_lng.uz2(v_lng)
-        logfile.write("%d %5.2f %6.2f %4.2f %8.2f %6.2f %7.3f %7.3f %7.3f %6.2f %6.2f %6.2f %5.2f %5.2f\n" %
-                      (tuple(xm) + (z_lng,)))
-        logfile.flush()
+            display_everything({'temp': temp, 'hum' : humidity, 'pres' : pressure,
+                                'pms010': x_pms[0], 'pms025': x_pms[1], 'pms100': x_pms[2] },
+                               bars = ((barify(z_act), 0, 0),
+                                       (0, barify(z_env), 0),
+                                       (0, 0, barify(z_gpm))),
+                               bg  = (0, 0, 0)) #(255*(proximity>0), 0, 0))
 
-    if i % (5*60) == 0:
-        pickle.dump((a_act, a_env, a_gpm, a_lng), open(INTEGRATORS_FILENAME, 'wb'))
-        
-    time.sleep(1.0)
-                    
+            # Logging
+            x = np.array((t0, temp, pressure, humidity, lux, proximity) +
+                             x_gas + x_pms + (z_act,), 'float')
+            xs.append(x) 
+            v_lngs.append(np.concatenate((v_gas, v_env, v_prs, v_pms, v_lgt)))
+
+        if i % 60 == 0 and i>1: # Skip the round with no data
+            xm = np.mean(xs, 0); xs = []
+            v_lng = np.mean(v_lngs, 0); v_lngs = []
+            z_lng = a_lng.uz2(v_lng)
+            logfile.write("%d %5.2f %6.2f %4.2f %8.2f %6.2f %7.3f %7.3f %7.3f %6.2f %6.2f %6.2f %5.2f %5.2f\n" %
+                          (tuple(xm) + (z_lng,)))
+            logfile.flush()
+
+        if i % (5*60) == 0 and i>1:
+            pickle.dump((a_act, a_env, a_gpm, a_lng), open(INTEGRATORS_FILENAME, 'wb'))
+
+asyncio.run(main())
+
 # TODO
 # - PMS error
 # - a bit more integrator saving, maybe to a shelve
